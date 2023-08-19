0598287ba0673abe3966d216beef28376a9e127b
Paul van der Zwan <paulz@vanderzwan.org>
2023-08-19 19:10:11 +0200

    Adapt for MK4

--- printer_handler.py
+++ printer_handler.py
@@ -1,6 +1,7 @@
 from paho.mqtt.client import Client
 from time import sleep, time
 import requests
+import pprint
 
 from json import dumps
 
@@ -47,11 +48,6 @@ class PrinterHandler:
         # set will
         self.printer_info = self.get_printer_info()
 
-        actual_status_topic = self.config_handler.get('mqtt_topics', 'job_progress_topic')
-        payload = self.get_print_progress_content(generate_last_wish=True)
-        print(f'Publishing to {actual_status_topic} with payload {payload}')
-        self.mqtt_client.will_set(actual_status_topic, payload=payload, qos=1, retain=True)
-
         self.mqtt_client.connect(config_handler.get('mqtt_broker', 'broker_ip'),
                                  config_handler.get_int('mqtt_broker', 'broker_port'))
 
@@ -89,7 +85,7 @@ class PrinterHandler:
 
         if generate_last_wish:
             print_status = {
-                'Printer': self.printer_info['name'],
+                'Printer': self.printer_info['hostname'],
                 'Job': 'Not printing',
                 'Elapsed_time_s': 0,
                 'Progress_percent': 0
@@ -97,15 +93,11 @@ class PrinterHandler:
         else:
             job_name = self.job_status['file']['name'] if self.job_status is not None else ""
             # if elapsed time is less than 1 second, replace job name with printer state
-            if self.printer_status is not None and "job" in self.printer_status is not None and "time_printing" in self.printer_status['job'] and self.printer_status['job']['time_printing'] < 1:
-                job_name = self.printer_status['printer']['state']
-
-            # also override it if we do not know the time printing
-            if self.printer_status is not None and "job" in self.printer_status is None or "time_printing" not in self.printer_status['job']:
+            if self.job_status is not None and "job" in self.printer_status is not None and "time_printing" in self.printer_status['job'] and self.printer_status['job']['time_printing'] < 1:
                 job_name = self.printer_status['printer']['state']
 
             print_status = {
-                'Printer': self.printer_info['name'],
+                'Printer': self.printer_info['hostname'],
                 'Job': job_name,
                 'Elapsed_time_s': self.printer_status['job']['time_printing'] if "job" in self.printer_status is not None and "time_printing" in self.printer_status['job'] else 0,
                 'Progress_percent': self.printer_status['job']['progress'] if "job" in self.printer_status is not None else 0
@@ -124,37 +116,10 @@ class PrinterHandler:
 
         return None
 
-    def custom_temp(self, value, location_key):
-        return dumps({
-            "Value": value,
-            "Location": self.config_handler.get('mqtt_topics', location_key),
-            "Ts": int(time())
-        })
-
     def publish_topics(self):
         topics = {
-            # job
-            'job_progress_topic': self.get_print_progress_content(),
-            'job_percentage_topic': self.printer_status['job']['progress'] if "job" in self.printer_status is not None else 0,
-            'job_name': self.job_status['file']['name'] if self.job_status is not None else "",
-            'job_elapsed_time_topic': self.job_status['time_printing'] if self.job_status is not None else "",
-            'job_estimated_time_topic': self.job_status['file']['meta']['estimated_print_time'] if self.job_status is not None else "",
-            'job_remaining_time_topic': self.job_status['time_remaining'] if self.job_status is not None else "",
-            'job_nozzle_diameter_topic': self.job_status['file']['meta']['nozzle_diameter'] if self.job_status is not None else "",
-            'job_layer_height_topic': self.job_status['file']['meta']['layer_height'] if self.job_status is not None else "",
-            'job_filament_used_mm_topic': self.job_status['file']['meta']['filament used [mm]'] if self.job_status is not None else "",
-            'job_filament_used_g_topic': self.job_status['file']['meta']['filament used [g]'] if self.job_status is not None else "",
-            'job_filament_type_topic': self.job_status['file']['meta']['filament_type'] if self.job_status is not None else "",
-            'job_infill_topic': self.job_status['file']['meta']['fill_density'] if self.job_status is not None else "",
-
-            # printer
-            'printer_status_topic': self.printer_status['printer']['state'],
-            'printer_nozzle_temp_topic': self.printer_status['printer']['temp_nozzle'],
-            'printer_bed_temp_topic': self.printer_status['printer']['temp_bed'],
-            'printer_target_nozzle_temp_topic': self.printer_status['printer']['target_nozzle'],
-            'printer_target_bed_temp_topic': self.printer_status['printer']['target_bed'],
-            'printer_custom_nozzle_temp_topic': self.custom_temp(self.printer_status['printer']['temp_nozzle'], 'custom_nozzle_location'),
-            'printer_custom_bed_temp_topic': self.custom_temp(self.printer_status['printer']['temp_bed'], 'custom_bed_location'),
+            'job_topic': dumps(self.job_status),
+            'printer_topic': dumps(self.printer_status)
         }
 
         for topic in topics:
